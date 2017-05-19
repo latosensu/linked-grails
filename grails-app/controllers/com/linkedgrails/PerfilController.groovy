6b1415e754544ab6c0ecfb9266992da4b403c6ee
Bruno <bruno.eng.comp@gmail.com>
2017-05-19 15:44:45 -0300

    Usar redirect

--- grails-app/controllers/com/linkedgrails/PerfilController.groovy
+++ grails-app/controllers/com/linkedgrails/PerfilController.groovy
@@ -5,6 +5,8 @@ import grails.converters.XML
 class PerfilController {
 
     def meuPerfil(Pessoa pessoa) {
-        respond pessoa, model: [informacaoExtra1: 'info1', informacaoExtra2: 'info2']
+        //respond pessoa, model: [informacaoExtra1: 'info1', informacaoExtra2: 'info2']
+//        redirect(url: "http://www.ufscar.br")
+        redirect(controller: "pessoa", action: "index", params: ['max':5])
     }
 }
