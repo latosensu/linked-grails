2ad273294923105c87878e945d21fa8187aa7ea7
Bruno <bruno.eng.comp@gmail.com>
2017-05-19 15:00:53 -0300

    Passar informacoes do controller para a view

--- grails-app/controllers/com/linkedgrails/PerfilController.groovy
+++ grails-app/controllers/com/linkedgrails/PerfilController.groovy
@@ -5,6 +5,6 @@ import grails.converters.XML
 class PerfilController {
 
     def meuPerfil(Pessoa pessoa) {
-        respond pessoa
+        respond pessoa, [model: [informacaoExtra1: 'info1', informacaoExtra2: 'info2']]
     }
 }
---
+++ grails-app/views/perfil/meuPerfil.gsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+<pre>
+Pessoa: ${pessoa}
+
+Informacao 1: ${informacaoExtra1}
+
+Informacao 2: ${informacaoExtra2}
+</pre>
+</body>
+</html>
