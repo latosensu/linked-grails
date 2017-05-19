d190e38525320ff8b73e4d478ccc389dcc6820ad
Bruno <bruno.eng.comp@gmail.com>
2017-05-19 16:00:58 -0300

    Utilizar variavle flash

--- grails-app/controllers/com/linkedgrails/PerfilController.groovy
+++ grails-app/controllers/com/linkedgrails/PerfilController.groovy
@@ -5,8 +5,7 @@ import grails.converters.XML
 class PerfilController {
 
     def meuPerfil(Pessoa pessoa) {
-        //respond pessoa, model: [informacaoExtra1: 'info1', informacaoExtra2: 'info2']
-//        redirect(url: "http://www.ufscar.br")
-        redirect(controller: "pessoa", action: "index", params: ['max':5])
+        flash.message = "Teste"
+        redirect(controller: "grupo", action: "index", params: ['max':5])
     }
 }
