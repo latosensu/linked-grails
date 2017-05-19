0709d98c0fdb19a5843fe01fbb2c0028a5f2ed79
Bruno <bruno.eng.comp@gmail.com>
2017-05-19 13:29:21 -0300

    Criar mapeamento no UrlMappings

--- grails-app/controllers/linked/grails/UrlMappings.groovy
+++ grails-app/controllers/linked/grails/UrlMappings.groovy
@@ -3,15 +3,22 @@ package linked.grails
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
+
+
+        "/$controller/$action?/$id?(.$format)?" {
             constraints {
                 // apply constraints here
             }
         }
 
-        "/"(view:"/index")
-        "500"(view:'/error')
-        "404"(view:'/notFound')
+        "/meuPerfil/$id"(
+                controller: 'perfil',
+                action: 'meuPerfil'
+        )
+
+        "/"(view: "/index")
+        "500"(view: '/error')
+        "404"(view: '/notFound')
 
 
     }
